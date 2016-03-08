b50e2edf3ad99757b747ea58245cd87865bee5c7
John Kayne <johnhoganuke2@gmail.com>
2016-03-08 10:43:58 -0500

    Create XItemGem.java

---
+++ src/main/java/snowpaw/projectx/world/item/XItemGem.java
@@ -0,0 +1,11 @@
+package snowpaw.projectx.world.item;
+
+import snowpaw.projectx.lib.item.XItemBase;
+
+public class XItemGem extends XItemBase {
+
+	public XItemGem(String itemName, int stackSize, String... subNames) {
+		super(itemName, stackSize, subNames);
+	}
+
+}
