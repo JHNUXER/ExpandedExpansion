b807fa16d30833d001112f2c32de83155597d907
John Kayne <johnhoganuke2@gmail.com>
2016-03-08 10:44:36 -0500

    Create XItemIngot.java

---
+++ src/main/java/snowpaw/projectx/world/item/XItemIngot.java
@@ -0,0 +1,11 @@
+package snowpaw.projectx.world.item;
+
+import snowpaw.projectx.lib.item.XItemBase;
+
+public class XItemIngot extends XItemBase {
+
+	public XItemIngot(String itemName, int stackSize, String... subNames) {
+		super(itemName, stackSize, subNames);
+	}
+
+}
