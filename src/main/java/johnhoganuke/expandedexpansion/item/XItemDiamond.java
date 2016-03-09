6b6370b2cf9dfc3bb0a615f30d2c8fc598b69253
John Kayne <johnhoganuke2@gmail.com>
2016-03-09 11:15:55 -0500

    Create XItemDiamond.java

---
+++ src/main/java/johnhoganuke/expandedexpansion/item/XItemDiamond.java
@@ -0,0 +1,11 @@
+package johnhoganuke.expandedexpansion.item;
+
+import snowpaw.projectx.lib.item.XItemBase;
+
+public class XItemDiamond extends XItemBase {
+
+	public XItemDiamond(String itemName, int stackSize, String... subNames) {
+		super(itemName, stackSize, subNames);
+	}
+
+}
