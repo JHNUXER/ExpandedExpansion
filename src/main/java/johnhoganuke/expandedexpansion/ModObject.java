d56787448b59980641a8b199369a8afb38635cf3
John Kayne <johnhoganuke2@gmail.com>
2016-03-10 10:50:10 -0500

    Create ModObject.java

---
+++ src/main/java/johnhoganuke/expandedexpansion/ModObject.java
@@ -0,0 +1,17 @@
+package johnhoganuke.expandedexpansion;
+
+public enum ModObject {
+// Machines:
+blockJohnEnergyCell,            // Best I could come up with! Don't judge me! :-(
+
+// Items:
+itemXycroniumDiamond,           // src/main/java/johnhoganuke/item/XItemDiamond.java
+
+
+public final String unlocalisedName;
+
+  private ModObject() {
+    this.unlocalisedName = name();
+  }
+
+}
