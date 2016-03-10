6123b602e9041d76cb8752fa897bc4d354d41ff4
John Kayne <johnhoganuke2@gmail.com>
2016-03-10 10:53:57 -0500

    Create CommonProxy.java

---
+++ src/main/java/johnhoganuke/expandedexpansion/CommonProxy.java
@@ -0,0 +1,12 @@
+package crazypants.enderio;
+
+import java.text.DecimalFormat;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.util.IIcon;
+// import snowpaw.projectx.core.ProjectX;
+import snowpaw.projectx.core.render.AnimationFX;
+import snowpaw.projectx.core.render.RenderSimpleGlow;
