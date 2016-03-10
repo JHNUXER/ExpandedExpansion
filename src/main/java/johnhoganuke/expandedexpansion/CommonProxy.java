963ba0e76cb336f662b92720023e9fe276b877c6
John Kayne <johnhoganuke2@gmail.com>
2016-03-10 10:56:16 -0500

    Update CommonProxy.java

--- src/main/java/johnhoganuke/expandedexpansion/CommonProxy.java
+++ src/main/java/johnhoganuke/expandedexpansion/CommonProxy.java
@@ -10,3 +10,55 @@ import net.minecraft.util.IIcon;
 // import snowpaw.projectx.core.ProjectX;
 import snowpaw.projectx.core.render.AnimationFX;
 import snowpaw.projectx.core.render.RenderSimpleGlow;
+
+public class CommonProxy {
+  
+  private static final DecimalFormat FORMAT = new DecimalFormat("########0.000");
+	public static AnimationFX animationFX = null;
+	public static int glowRenderId;
+
+  public CommonProxy() {
+  }
+
+  public World getClientWorld() {
+    return null;
+  }
+
+  public EntityPlayer getClientPlayer() {
+    return null;
+  }
+
+  //public ConduitRenderer getRendererForConduit(IConduit conduit) {
+  //  return null;
+  //}
+
+  public double getReachDistanceForPlayer(EntityPlayer entityPlayer) {
+    return 5;
+
+  }
+
+  public void load() {
+  }
+  
+	@Override
+	public void preInit(){
+		registerIcons();
+		glowRenderId = RenderingRegistry.getNextAvailableRenderId();
+	}
+	
+	@Override
+	public void init(){ 
+
+		RenderingRegistry.registerBlockHandler(new RenderSimpleGlow());
+	}
+	
+	@Override
+	public void postInit(){
+		
+	}
+	
+	public void registerIcons(){
+		animationFX = new AnimationFX();
+	}
+
+}
