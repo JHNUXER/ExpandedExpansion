203d39ac074df2a24b7180ff7cd5f8f7a7c345cd
John Kayne <johnhoganuke2@gmail.com>
2016-03-14 11:01:00 -0400

    Added Adamantium

--- src/main/java/johnhoganuke/expandedexpansion/ModObject.java
+++ src/main/java/johnhoganuke/expandedexpansion/ModObject.java
@@ -4,8 +4,12 @@ public enum ModObject {
 // Machines:
 blockJohnEnergyCell,            // Best I could come up with! Don't judge me! :-(
 
+// Blocks:
+blockAdamantium,
+
 // Items:
 itemXycroniumDiamond,           // src/main/java/johnhoganuke/item/XItemDiamond.java
+itemAdamantiumIngot,
 
 
 public final String unlocalisedName;
